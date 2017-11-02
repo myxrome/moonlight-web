35dbe185bced91d844c8985d6101a183aca20eda
Aleksey Zolotov <myxrome@gmail.com>
2017-11-02 17:06:56 +0400

    MOON-7 - инициализация проекта.

---
+++ config/initializers/her.rb
@@ -0,0 +1,10 @@
+Her::API.setup url: ENV['MODEL_SERVICE_URL'] do |c|
+  # Request
+  c.use Faraday::Request::UrlEncoded
+
+  # Response
+  c.use Her::Middleware::DefaultParseJSON
+
+  # Adapter
+  c.use Faraday::Adapter::NetHttp
+end
